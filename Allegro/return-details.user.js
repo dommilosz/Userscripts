4a91bea19237529a33c1d9a4488bf9047479ea34
dommilosz <dommilosz@gmail.com>
2024-08-28 20:59:14 +0200

    Update return-details.user.js

--- Allegro/return-details.user.js
+++ Allegro/return-details.user.js
@@ -3,7 +3,7 @@
 // @namespace   Violentmonkey Scripts
 // @match       https://allegro.pl/moje-allegro/zakupy/kupione
 // @grant       none
-// @version     1.4
+// @version     1.5
 // @author      dommilosz
 // @description 10/22/2023, 2:13:18 PM
 // @updateURL https://github.com/dommilosz/Userscripts/raw/master/Allegro/return-details.user.js
@@ -15,43 +15,64 @@ async function delay(ms){
   return await new Promise(r=>setTimeout(r, ms));
 }
 
-function getReturnInfo(el){
-  let returnStatus = el.querySelector("#opbox-myorder-returns > div > div > div > div");
-  let returnTimes = [...(el.querySelector(`button[data-button-type="return"]`).parentNode.querySelectorAll("span"))];
-
-  let returnTime = document.createElement("div");
-  returnTime.style.display = "flex";
-  returnTime.style.flexDirection = "column";
-  returnTimes.forEach(el=>{
-    returnTime.appendChild(el)
-  })
-
-  if(!returnStatus){
-    returnStatus = document.createElement("div");
-    returnStatus.outerHTML = `<div class="mpof_ki mwdn_1 mr3m_1 gvl9v">Brak zwrotu</div>`
+function addProperty(id,title, value, orderDiv){
+  let child;
+  if(orderDiv.querySelector(`#prop-id-${id}`) != null){
+    child = orderDiv.querySelector(`#prop-id-${id}`);
+  }else{
+    child = document.createElement('div');
+    orderDiv.appendChild(child);
   }
 
-  returnStatus.className = "mpof_ki mwdn_1 mr3m_1 gvl9v msts_pt munh_0 mj7a_16 mg9e_16 m389_6m";
 
-  returnStatus.appendChild(returnTime);
-  returnTime.className = "mpof_ki mr3m_1 mjyo_6x gel0f";
+  child.innerHTML=`<div id="prop-id-${id}" class="myre_zn myre_8v_l" bis_skin_checked="1"><div class="mpof_ki m7f5_sf m389_a6 _355a6_H7r-d" bis_skin_checked="1"><div class="mpof_ki m389_a6 mwdn_1" bis_skin_checked="1"><p id="total-price-label-bd38004c-1b9e-30ae-81e0-55ec852c7ddd" class="m9qz_yr msa3_z4 mryx_0 mp4t_0 m3h2_8">${title}</p><div class="mpof_ki mr3m_1 mjyo_6x gel0f mh36_0" bis_skin_checked="1"></div></div><div id="total-price-bd38004c-1b9e-30ae-81e0-55ec852c7ddd" class="m9qz_yr" bis_skin_checked="1"><span class="mli8_k4 msa3_z4 mqu1_1 mp0t_0a mgmw_qw mgn2_14">${value}</span></div></div></div>`
 
-  return returnStatus;
+
 }
 
-async function loadDetails(url){
-  let details = await (await fetch(url)).text();
-  var el = document.createElement( 'html' );
-  el.innerHTML = details;
-  window.el = el;
+let details = {};
 
-  let banner = document.createElement("div");
+async function fetchDetails(groupId){
+  let detailsRes = await fetch(`https://edge.allegro.pl/myorder-api/myorders/${groupId}`, {
+    "headers": {
+        "Content-Type": "application/vnd.allegro.public.v3+json",
+        "Accept": "application/vnd.allegro.public.v3+json",
+    },
+    "method": "GET",
+});
 
-  let returnInfo = getReturnInfo(el);
+  let details = (await detailsRes.json()).myorders[0];
 
-  banner.appendChild(returnInfo);
+  return details;
+}
 
-  return  banner;
+
+async function processGroup(groupId){
+  if(!details[groupId]){
+    details[groupId] = await fetchDetails(groupId);
+  }
+  let group = details[groupId];
+
+  let div = document.querySelector(`#group-id-${group.groupId}`)
+  let orderDiv = document.querySelector(`#group-id-${group.groupId}>div>div:has(section)`);
+
+  try{
+  addProperty("returns","Returns",group.myorders[0].status.actions.filter(el=>el.type=="RETURN_PRODUCTS")[0].details.filter(el=>el.type=="TEXT").map(el=>el.value).join("<br>"),orderDiv);
+}catch{}
+
+  try{
+  group.timelines.forEach(waybill=>{
+    addProperty("parcel-"+waybill.waybillId, "Parcel", waybill.waybillId, orderDiv);
+  })}catch{}
+
+  try{
+  group.rescissions.rescissions.forEach(waybill=>{
+    addProperty("return-"+waybill.rescissionId, "Return Status", waybill.timelineStatus.label + " " + waybill.timelineStatus.hint, orderDiv);
+  })}catch{}
+
+  try{
+  addProperty("return-lbl", "Return", group.status.actions.filter(el=>el.type=="RETURN_PRODUCTS")[0].details.filter(el=>el.type=="TEXT").map(el=>el.value).join(" "), orderDiv);
+  }catch{}
 }
 
 async function main(){
@@ -61,51 +82,30 @@ async function main(){
     await delay(500);
   }
 
+  let groups = [...document.querySelectorAll("div.mryx_16")].filter(el=>el.id.startsWith('group-id-')).map(el=>el.id.replace("group-id-",''));
+  groups.forEach(processGroup)
+
   let orders = [...document.querySelectorAll("#my-orders-listing > span > div")].filter(el=>el.id.startsWith("group-id-"))
   for(let order of orders){
     let url = [...order.querySelectorAll("a")].filter(el=>el.href.includes("/moje-allegro/zakupy/kupione/"))[0].href;
     let group = order.children[0];
 
-    if(group.className.includes("button-added")){
-      if(document.querySelector("#pending_returns_li_btn")?.checked){
+    if(document.querySelector("#pending_returns_li_btn")?.checked){
         let content = order.textContent;
-        if(content.includes("Zwrot zakończony") || content.includes("Przesyłka odebrana")){
+        if(content.includes("Decyzja sprzedającego") || content.includes("Przesyłka odebrana") || content.includes("anulowany")){
           order.style.display="none";
         }
       }else if (document.querySelector("#notfull_returns_li_btn")?.checked){
         let content = order.textContent;
-        if(!content.includes("Zwrot") || content.includes("Pełen zwrot pieniędzy")){
+        if(!content.includes("Return Status") || content.includes("Pełen zwrot pieniędzy")){
           order.style.display="none";
         }
       }else{
         order.style.display="";
       }
       continue;
-    }
-    group.classList.add("button-added");
-
-    let button = document.createElement("button");
-    button.innerHTML = "Fetch details";
-    button.onclick = ftch;
-
-    let header = order.querySelector("div:nth-child(1) > div:nth-child(1) > div:nth-child(2)");
-    header.appendChild(button);
-
-    async function ftch(){
-      let banner = await loadDetails(url, header.parentNode);
-
-      let childBefore = group.children[1];
-
-      group.insertBefore(banner, childBefore);
-      let hr = document.createElement("hr");
-      hr.className = `mpof_z0 m7er_k4 mp4t_0 m3h2_0 mryx_0 munh_0 m911_5r mefy_5r mnyp_5r mdwl_5r mse2_1 msts_me`
-      group.insertBefore(hr, banner);
-      button.style.display = "none";
-    }
-    if(autoRun){
-      ftch();
-    }
   }
+
 
   if(!document.querySelector("#pending_returns_li_btn")){
     let li = document.createElement("li");
